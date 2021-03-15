93a50f12fedfc813c86d4ee691b19d011a29aaab
Mootook <alexanderkmutuc@gmail.com>
2021-03-14 17:12:23 -0700

    Project 04, Hack assembly Programs

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,77 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudo-Code
+// infinite loop
+// check kecode > 0 
+// goto FILL
+// else goto EMPTY
+
+// addr = SCREEN (beginning register of Sreen Memory Map)
+// n = RAM[0]
+// i = 0
+
+// LOOP:
+// if i > n goto END
+// RAM[addr] = -1 1111111111111111
+
+// advances to next row
+// addr = addr + 32
+// i = i + 1
+// goto LOOP
+
+// Code:
+
+(START)
+  // Screen is 512 x 255
+  // First 32 "word"s 16bit line, corresponse to first row of display unit
+
+  @8192 // (32 * 256) number of 16 bit lines needed for fill/emptying (End of Screen)
+        // (32 16bit sequences for each row, 256 rows)
+  D=A
+  @addr
+  M=D // addr now has the 8192 val, starting any loop with the value
+      // of the number of bits needed for processing
+  
+(KEYPOLL)
+  @addr
+  M=M-1 // substract 1 from the address to go backwards 
+        // if this is less than 0, we've processed every bit and can restart
+  D=M
+
+  @START
+  D;JLT // if the address is less than 0, reinitailize
+
+  @KBD
+  D=M // Read the value stored at KBD
+
+  @EMPTY
+  D;JEQ // if keycode is zero, empty the screen
+
+  @FILL
+  D;JGT // if D > 0, FILL
+
+(FILL)
+  @SCREEN
+  D=A // store address of SCREEN register in D
+  @addr
+	A=D+M   // add current address val onto screen register
+  M=-1  // set value of current address 1111111111111111 BLACK
+
+  @KEYPOLL
+  0;JMP
+
+(EMPTY)
+  @SCREEN
+  D=A
+  @addr
+  A=D+M
+  M=0
+
+  @KEYPOLL
+  0;JMP
+  
+  
+
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,76 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+// Pseudo-code
+// for (let i = 0; i <  R1; i++) {
+//   result = result + R0
+// }
+// Add R0, R1 times.
+//
+// n = R1
+// product = 0
+// i = 0
+// LOOP:
+//   if i > n goto STOP
+//   product = procuct + R0
+//   i = i + 1
+//   goto LOOP
+// STOP:
+//  R2 = product
+
+
+// CODE
+  @R0 // select 0 register
+  D=M // store contents of 0 register in D
+  @n  // variable n
+  M=D // n = R0 store value of R0 in n variable
+
+  @i  // create and select variable i 
+  M=1 // initialize the selcted register (i) as 1
+
+  @product  // store the result in a temporary variable at the next available register
+  M=0
+
+(LOOP)
+  @i
+  D=M // D = i
+  @R1
+  D=D-M // D = i - R1 , how many times we're adding R0 to itself
+  @STOP
+  D;JGT // if i > R1 goto STOP
+
+  @n
+  D=M // store what to add
+  @product
+  M=D+M // get the cycle's product result
+
+  @i
+  M=M+1 // increment for our loop
+
+  @LOOP
+  0;JMP //start the loop again
+
+
+(STOP)
+  @product
+  D=M
+  @R2
+  M=D // RAM[2] = product
+
+(END)
+  @END
+  0;JMP
+
+
+
+
+
+
+
+
+
+
+
+
